b7544a7518b2d42bee1ffe73d92e6972f3b7041d
win11905 <30414878+win11905@users.noreply.github.com>
2018-08-02 14:25:02 +0700

    update : 2/8/18

--- JOIOC/2018/xylophone/cpp/xylophone.cpp
+++ JOIOC/2018/xylophone/cpp/xylophone.cpp
@@ -2,11 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static int A[5000];
+int n, A[5005];
+
+int getNum(int a1, int a2, int a, int b) {
+	if(a < b) {
+		if(a2 - a1 == b - a) return a - a1;
+		return a + a1;
+	} else {
+		if(a2 - a1 == a - b) return a + a1;
+		return a - a1;
+	}
+}
 
 void solve(int N) {
-	int value = query(1, N);
-	for(int i = 1; i <= N; i++) {
-		answer(i, i);
+	n = N;
+	A[1] = 0, A[2] = query(1, 2);
+	for(int i = 3; i <= n; ++i) A[i] = getNum(query(i-1, i), query(i-2, i), A[i-1], A[i-2]);
+	int mn = *min_element(A+1, A+n+1);
+	int p1, pn;
+	for_each(A+1, A+n+1, [&](int &x) { x = x - mn + 1; });
+	for(int i = 1; i <= n; ++i) {
+		if(A[i] == 1) p1 = i;
+		if(A[i] == n) pn = i;
 	}
+	if(p1 > pn) for_each(A+1, A+n+1, [&](int &x) { x = n - x + 1; });
+	for(int i = 1; i <= n; ++i) answer(i, A[i]);
 }
