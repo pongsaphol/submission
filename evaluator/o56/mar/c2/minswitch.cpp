4351a19a3a9bcbc61efa600138d9b33beec6a07c
win11905 <donothavemoney@gmail.com>
2018-08-30 23:43:45 +0700

    update : 30/8/18

--- evaluator/o56/mar/c2/minswitch.cpp
+++ evaluator/o56/mar/c2/minswitch.cpp
@@ -1,40 +1,14 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-#define long long long 
-#define P pair<int, int>
-#define x first
-#define y second
-#define mem(x) memset((x), 0, sizeof(x))
-#define all(x) (x).begin(), (x).end()
-#define input() (*istream_iterator<int>(cin))
-#ifdef INPUT
-#define dbg(x) cout << "debug : " <<  x << endl
-#define dbg2(x, xx) cout << "debug : " << x << " : " << xx << endl 
-#else
-#define dbg(x)
-#define dbg2(x, xx)
-#endif
-using namespace __gnu_pbds;
 using namespace std;
-template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
-void red() {
-	#ifdef INPUT
-	freopen("r", "r", stdin);
-	#endif
-	cin.tie(0);
-	cin.sync_with_stdio(false);
-}
-
-const int MAXN = 1e5+5;
+const int N = 1e4+5;
 
 int n, m, mn = 21;
-vector<int> g[MAXN], ans;
-bool chk[MAXN];
+vector<int> g[N], ans;
+bool chk[N];
 
 void dfs(int u, int cnt) {
-	if(cnt > mn) return;
+	if(cnt >= mn) return;
 	if(u > n) {
 		if(cnt < mn) {
 			mn = cnt;
@@ -55,7 +29,7 @@ void dfs(int u, int cnt) {
 			dfs(u+1, cnt + ret.size());
 			for(auto v:ret) chk[v] = false;
 		}
-		if(g[u].size() > 1) {
+		if(g[u].size() > 2) {
 			chk[u] = true;
 			dfs(u+1, cnt+1);
 			chk[u] = false;
@@ -64,7 +38,6 @@ void dfs(int u, int cnt) {
 }
 
 int main() {
-	red();
 	scanf("%d %d", &n, &m);
 	while(m--) {
 		int a, b;
