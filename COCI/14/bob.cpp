60966b5f8edfa97d5e758a3e9f6c07c90346ca83
win11905 <donothavemoney@gmail.com>
2019-02-14 14:20:32 +0700

    update

---
+++ COCI/14/bob.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define vi vector<int>
+#define iii tuple<int, int, int>
+#define long long long
+#define pii pair<int, int>
+#define x first
+#define y second
+using namespace std;
+const long MOD = 1e9+7, LINF = 1e18 + 1e16;
+const int INF = 1e9+1;
+const double EPS = 1e-10;
+const int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+const int N = 1005;
+
+class bob {
+private:
+    int n, m;
+    int arr[N][N], pref[N][N];
+    long ans;
+public:
+    void solve(istream& cin, ostream& cout) {
+        cin >> n >> m;
+        for(int i = 1; i <= n; ++i) for(int j = 1; j <= m; ++j) {
+            cin >> arr[i][j];
+            pref[i][j] = 1 + (arr[i][j] == arr[i-1][j] ? pref[i-1][j] : 0);
+        }
+        for(int i = 1; i <= n; ++i) {
+            int cnt = 0, ft = 0;
+            stack<pii> stk; // pos, sum;
+            for(int j = 1; j <= m; ++j) {
+                if(arr[i][j] != arr[i][j-1]) while(!stk.empty()) stk.pop(), cnt = 0, ft = j-1;
+                int now = pref[i][j];
+                while(!stk.empty() && pref[i][stk.top().x] >= pref[i][j]) {
+                    int z = stk.top().x; cnt -= stk.top().y;
+                    stk.pop(); now += pref[i][j] * (z - (stk.empty() ? ft : stk.top().x));
+                }
+                cnt += now, stk.emplace(j, now);
+                ans += cnt;
+            }
+        }
+        cout << ans << endl;
+    }
+};
