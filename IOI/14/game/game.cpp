53afea10ffd26e01cf0d96e53e2b421140a0e09d
Pongsaphol Pongsawakul <pongsaphol@MacBook-Pro-khxng-Pongsaphol.local>
2019-06-30 12:37:48 +0700

    update

--- IOI/14/game/game.cpp
+++ IOI/14/game/game.cpp
@@ -1,28 +1,31 @@
 #include "game.h"
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
 
-set<int> S[1505];
+const int N = 1500;
+
+int n;
+int par[N];
+int adj[N][N];
+
+int find(int u) { return par[u] = par[u] == u ? u : find(par[u]); }
 
 void initialize(int n) {
-    for(int i = 0; i < n; ++i) for(int j = 0; j < n; ++j) if(i != j) {
-        S[i].emplace(j);
-    }
+    iota(par, par+N, 0), ::n = n;
+    for(int i = 0; i < n; ++i) for(int j = 0; j < n; ++j) if(i != j) adj[i][j] = 1;
 }
 
 int hasEdge(int u, int v) {
-    bool st = false;
-    if(S[u].count(v)) S[u].erase(v);
-    else st = true;
-    if(S[v].count(u)) S[v].erase(u);
-    else st = true;
-    if(S[u].size() == 1) {
-        int a = *S[u].begin();
-        S[a].erase(u);
-    } 
-    if(S[v].size() == 1) {
-        int a = *S[v].begin();
-        S[a].erase(v);
+    u = find(u), v = find(v);
+    assert(u != v);
+    if(adj[u][v] == 1) {
+        adj[u][v] = adj[v][u] = 0;
+        par[v] = u;
+        for(int i = 0; i < n; ++i) adj[u][i] += adj[v][i], adj[v][i] = 0;
+        for(int i = 0; i < n; ++i) if(par[i] == i && i != u) adj[i][u] += adj[i][v], adj[i][v] = 0;
+        return 1;
+    } else {
+        adj[u][v]--, adj[v][u]--;
+        return 0;
     }
-    return st;
 }
