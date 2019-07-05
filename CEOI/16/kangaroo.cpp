849bff1f4b9ca5ae3821d8b1aba8e7cc96520763
win11905 <donothavemoney@gmail.com>
2019-07-05 17:22:40 +0700

    update

---
+++ CEOI/16/kangaroo.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define long long long
+using namespace std;
+
+const int M = 1e9+7;
+
+const int N = 2e3+5;
+
+int n, s, t;
+long dp[N][N];
+
+long solve(int i, int j) {
+    if(j == 0) return 0;
+    long &now = dp[i][j];
+    if(now != -1) return now;
+    if(i == 1) return now = (j == 1 ? 1 : 0);
+    if(i == s || i == t) 
+        return now = (solve(i-1, j-1) + solve(i-1, j)) % M;
+    return now = (solve(i-1, j+1) * j + solve(i-1, j-1) * (j - (i > s ? 1 : 0) - (i > t ? 1 : 0))) % M; 
+}
+
+int main() {
+    memset(dp, -1, sizeof dp);
+    scanf("%d %d %d", &n, &s, &t);
+    printf("%lld\n", solve(n, 1));
+}
