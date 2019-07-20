eaaa72476a9702025a4797d0911b2bd88bb4fdb7
Pongsaphol Pongsawakul <30414878+win11905@users.noreply.github.com>
2019-07-20 14:18:26 +0700

    Update 1C.cpp

--- OCN/16/1C.cpp
+++ OCN/16/1C.cpp
@@ -1,64 +1,35 @@
 #include <bits/stdc++.h>
-#define long long long
+#define long long long 
 using namespace std;
 
-const int N = 105;
+const long M = 1e9+7;
+const int N = 1e6+5;
 
-char re[N];
-int d[N][N];
-int n, m, l;
+string str;
+long n, m, k;
 
-int check() {
-    vector<int> vec;
-    for(int i = 1; i < n; ++i) vec.emplace_back(i);
-    set<long> S;
-    do {
-        long sum = d[vec[0]][n] + d[vec.back()][n];
-        for(int i = 1; i < vec.size(); ++i) sum += d[vec[i]][vec[i-1]];
-        S.emplace(sum);
-    } while(next_permutation(vec.begin(), vec.end()));
-    return S.size();
+long powz(long a, long b) {
+    long ret = 1;
+    for(; b; b >>= 1) {
+        if(b & 1) ret = ret * a % M;
+        a = a * a % M;
+    }
+    return ret;
 }
 
+long frac[N], ifrac[N];
+
 int main() {
-    srand(time(NULL));
-    scanf("%s", re);
-    scanf("%d %d %d", &n, &m, &l);
-    int z = 10;
-    double lll = l;
-    int divi = l / z;
-    l += l;
-    int delta = 0;
-    int zz = 0;
-    int k = 1;
-    long mx = 0;
-    int ll = 56250;
-    while(true) {
-        delta++;
-        // if(delta == 10) delta = 0, l += divi, cerr << "next" << endl;
-        if(delta % 100 == 0) cerr << "next" << endl, l += divi;
-        for(int i = 1; i <= n; ++i) {
-            for(int j = i+1; j <= n; ++j) {
-                d[i][j] = d[j][i] = (rand() % ll) + 1;
-            }
-        }
-                // cerr << "->" << S.size() << endl;
-        int v = check();
-        mx = max(mx, (long)v);
-        if(delta % 10 == 0) cerr << "max => " << mx << endl;
-        if(v >= m) {
-            cerr << "========== YES ===========" << endl;
-            int mx = 0;
-            for(int i = 1; i <= n; ++i) {
-                for(int j = 1; j <= n; ++j) {
-                    printf("%d ", d[i][j]);
-                    mx = max(mx, d[i][j]);
-                }
-                printf("\n");
-            }
-            cerr << "score : " << ((lll / mx) * 25) << endl;
-            break;
-        }
-    }
-    
+    frac[0] = ifrac[0] = 1;
+    for(int i = 1; i < N; ++i) ifrac[i] = powz(frac[i] = frac[i-1] * i % M, M-2);
+    for(int i = 0; i < N; ++i) assert(frac[i] * ifrac[i] % M == 1);
+    cin >> str;
+    cin >> n >> m >> k;
+    cin >> str;    
+    long z = m * (n-1);
+    long val = powz(2, z);
+    val = val * frac[m] % M; 
+    val = val * ifrac[m-k] % M; 
+    val = val * ifrac[k] % M; 
+    printf("%lld\n", val);
 }
